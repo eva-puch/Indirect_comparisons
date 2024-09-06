7832aeb7666bd476f27a946e864d5a0f4f3f92cf
eva-puch <eva.puch@yahoo.fr>
2024-09-06 20:55:51 +0200

    03 prop score description

--- Rscripts/03_propensity-score.R
+++ Rscripts/03_propensity-score.R
@@ -14,3 +14,32 @@ model <- glm(trt ~ sex + age + diag + therapies, data=data, family=binomial)
 summary(model)
 
 data$prop_score <- predict(model, type="response")
+
+#--------------------   Propensity score: description   ------------------------
+
+by(data$prop_score, data$trt, summary)
+
+# boxolot
+plot_ly(data,
+        x = ~prop_score,
+        y = ~trt,
+        type = "box",
+        orientation = "h",
+        boxpoints = FALSE,
+        color = ~trt,
+        colors = c(colT2,colT1)) %>%
+  layout(xaxis = list(range = c(-0.1, 1), title = "Propensity Score"),
+         yaxis = list(title = ""),
+         legend = list(
+           title = list(text = "Treatment"),
+           traceorder = "reversed"))
+
+# density
+ps <- ggplot(data, aes(x = prop_score, fill = trt)) +
+  geom_density(alpha = 0.5) +
+  labs(x = "Propensity score", y = "") +
+  theme_minimal() +
+  scale_fill_manual(name = "Treatment", values = c(colT1,colT2), breaks = c("T1", "T2")) +
+  theme(legend.position = "left") +
+  guides(fill = guide_legend(title = "Treatment"))
+ps
